a94a882f3692aa2b48bb0d89e312f27ee0b4fc30
Alexis Norberg <alexisnorberg@hotmail.com>
2022-05-20 13:41:19 +0200

    added new models for managing favourites

--- models/stations.ts
+++ models/stations.ts
@@ -1,4 +1,5 @@
 import config from "../config/config.json";
+import storage from "./storage";
 
 const stations = {
     getStations: async function getStations() {
@@ -6,6 +7,57 @@ const stations = {
     const result = await response.json();
 
     return result.data;
+    },
+    addFavouriteStation: async function addFavouriteStation(acronym: string) {
+        const token = await storage.readToken();
+
+        var data = {
+            artefact: `${acronym}`,
+            api_key: config.api_key
+        };
+
+        const response = await fetch(`${config.base_url}/data`, 
+        {
+            body: JSON.stringify(data),
+            headers: {
+                'content-type': 'application/json',
+                'x-access-token': token.token
+            },
+            method: 'POST'
+        });
+
+        const result = await response.json();
+    },
+    getFavouriteStation: async function getFavouriteStation() {
+        const token = await storage.readToken();
+
+        const response = await fetch(`${config.base_url}/data?api_key=${config.api_key}`, 
+        {
+            headers: {
+                'x-access-token': token.token
+            }
+        });
+
+        const result = await response.json();
+
+        return result.data;
+    },
+    deleteFavouriteStation: async function deleteFavouriteStation() {
+        const token = await storage.readToken();
+        var data = {
+            id: 1807,
+            api_key: config.api_key
+        };
+
+        const response = await fetch(`${config.base_url}/data`, 
+        {
+            body: JSON.stringify(data),
+            headers: {
+                'content-type': 'application/json',
+                'x-access-token': token.token
+            },
+            method: 'DELETE',
+        });
     }
 }
 
