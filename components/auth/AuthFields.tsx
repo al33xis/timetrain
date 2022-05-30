e771a65fe781ec13bc631760e88b89b8382af59d
Alexis Norberg <alexisnorberg@hotmail.com>
2022-05-30 17:47:10 +0200

    updated design and functionality, check if email or password is spelled accordingly

--- components/auth/AuthFields.tsx
+++ components/auth/AuthFields.tsx
@@ -1,6 +1,12 @@
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, TextInput, Button, Pressable, ScrollView } from "react-native";
 // import { Typography, Forms, Base } from "../../styles";
 import { showMessage } from "react-native-flash-message";
+import { StyleSheet } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+
+
+// sätt "default" värden på fälten
+
 
 //                                  dict  setDict         func
 export default function AuthFields({auth, setAuth, title, submit, navigation}) {
@@ -16,7 +22,6 @@ export default function AuthFields({auth, setAuth, title, submit, navigation}) {
                 description: "Ser inte ut som en emailadress.",
                 type: "warning",
                 floating: true,
-                autoHide: false
             });
         } else {
             showMessage({
@@ -44,10 +49,9 @@ export default function AuthFields({auth, setAuth, title, submit, navigation}) {
         if (!text.match(pattern)) {
             showMessage({
                 message: "Lösenord ej giltigt",
-                description: "Lösenordet måste innehålla minst en stor bokstav, en liten bokstav, en siffra, ett specialtecken(!.-) och vara minst 4 tecken långt.",
+                description: "Lösenordet uppfyller inte nedan krav.",
                 type: "warning",
                 floating: true,
-                autoHide: false
             });
         } else {
             showMessage({
@@ -58,53 +62,112 @@ export default function AuthFields({auth, setAuth, title, submit, navigation}) {
         }
     }
 
-    return (
-        <View>
-        <Text>{title}</Text>
 
-        <Text>E-post</Text>
+
+    return (
+        <ScrollView>
+        <View style={style.container}>
+        <Text style={style.header}>{title}</Text>
+
+        <Text style={style.header2}>E-post</Text>
         <TextInput
             onChangeText={(content: string) => {
-                validateEmail(content)
                 setAuth({...auth, email: content})
+            }}
+            onEndEditing={() => {
+                validateEmail(auth.email);
             }}
             value={auth?.email}
             keyboardType="email-address"
             autoCapitalize="none"
             autoCorrect={false}
             testID="email-field"
+            style={style.field}
             />
 
-        <Text>Lösenord</Text>
+        <Text style={style.header2}>Lösenord</Text>
         <TextInput 
             onChangeText={(content: string) => {
-                validatePassword(content)
                 setAuth({...auth, password: content})
+            }}
+            onEndEditing={() => {
+                validatePassword(auth.password);
             }}
             value={auth?.password}
             secureTextEntry={true}
             autoCapitalize="none"
             autoCorrect={false}
             testID="password-field"
+            style={style.field}
             />
+        <Text style={style.field_text}>Lösenordet måste innehålla: {"\n"} en stor bokstav {"\n"} en liten bokstav {"\n"} en siffra
+        {"\n"} ett specialtecken (!.-) {"\n"} och vara minst 4 tecken långt.</Text>
 
-        <Button 
-            title={title}
-            onPress={() => {
-                submit();
-            }}
-            accessibilityLabel={`${title} genom att trycka`}
-            />
-        <Text></Text>
+
+        <Pressable style={style.button} onPress={() => {
+            submit();
+        }}>
+            <Text style={style.button_text}>{title}</Text>
+        </Pressable>
+
         {title == "Logga in" &&
-            <Button 
-                title="Registrera"
-                onPress={() => {
-                    navigation.navigate("Register")
-                }}
-            />
+            <Pressable style={style.button} onPress={() => {
+                navigation.navigate("Register")
+            }}>
+                <Text style={style.button_text}>Registrera</Text>
+            </Pressable>
         }
 
+        {title == "Registrera" &&
+           <Pressable onPress={() => {
+                navigation.navigate("Login")
+            }}>
+                <Ionicons name="arrow-back-circle-outline" color={"#000"} size={50} style={{margin: 20}}/>
+            </Pressable>
+        }
     </View>
+    </ScrollView>
     );
 };
+
+const style = StyleSheet.create({
+    container: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    },
+    header: {
+        fontSize: 28,
+        fontWeight: "bold",
+        marginBottom: 22,
+    },
+    header2: {
+        fontSize: 18,
+    },
+    field_text: {
+        fontSize: 12,
+        width: 180,
+    },
+    field: {
+        width: 240,
+        margin: 22,
+        padding: 8,
+        fontSize: 18,
+        backgroundColor: "#FFF"
+    },
+    button: {
+        backgroundColor: "#00A438",
+        marginTop: 20,
+        padding: 10,
+        width: 300,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+    },
+    button_text: {
+        textAlign: "center",
+        fontSize: 20,
+        color: "#FFF"
+    }
+})
--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -12,13 +12,26 @@ export default function Login({ navigation, setIsLoggedIn }) {
             const result = await authModel.login(auth.email, auth.password);
             if (result.type === "success") {
                 setIsLoggedIn(true);
+                showMessage({
+                    message: "Välkommen!",
+                    description: "Du är nu inloggad.",
+                    type: "success",
+                    floating: true
+                });
+            } else {
+                showMessage({
+                    message: "Åtkomst nekad",
+                    description: "Inloggninsuppgifterna stämmer inte överens.",
+                    type: "danger",
+                    floating: true
+                });
             }
-            showMessage(result);
         } else {
             showMessage({
                 message: "Något saknas",
                 description: "E-post eller lösenord saknas",
                 type: "warning",
+                floating: true
             });
         }
     }
