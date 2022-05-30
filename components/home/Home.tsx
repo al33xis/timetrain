b5b5c853200f2c028efe92c122ad76415e561349
Alexis Norberg <alexisnorberg@hotmail.com>
2022-05-30 17:48:35 +0200

    updated design

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,15 +1,27 @@
 import { Image, Text, View, ScrollView } from 'react-native';
+import { StyleSheet } from "react-native";
+
 // import { Base, Typography } from "../../styles/index";
 
 export default function Home() {
 
     return (
         <ScrollView>
-            <View>
+            <View style={style.container}>
                 <Text>
-                    Startsida
+                    Välkommen till TimeTrain - din nya resekompanjon!
                 </Text>
+                
             </View>
         </ScrollView>
   );
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    }
+})
